c2607fb395055b600e61aba98688146c851f79d8
Kai Jie <hogrider0313@gmail.com>
2024-05-23 00:57:06 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+        child: Image.asset('images/win.png'),
+      ),
+    );
+  }
+}
